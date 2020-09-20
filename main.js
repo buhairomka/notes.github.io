d3c102aba71775d5a61a31d18545d06ef79ad784
buhairomka <buhairomka@ukr.net>
2020-09-20 22:36:31 +0300

    local storage

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const button_plus = document.getElementById('plus');
 const button_minus = document.getElementById('minus');
-const button_edit = document.getElementById('edit');
 const textarea = document.getElementById('textarea');
 let selected = null;
 let notes = [];
 
+window.onbeforeunload = () => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+window.onload = () => {
+    if (localStorage.getItem('notes')) {
+        notes = JSON.parse(localStorage.getItem('notes'))
+        console.log(notes)
+        notes.forEach((elem) => {
+            const lishka = document.createElement('li');
+            lishka.innerHTML = elem.name+'<br>';
+            lishka.setAttribute('class','');
+            lishka.setAttribute('id',elem.id);
+            let ulka = document.getElementById('ulka');
+            ulka.appendChild(lishka);
+        })
+        selected = null;
+    }
+}
 
 class Note {
     constructor(id, name, text, isSelected, date) {
@@ -18,9 +35,9 @@ class Note {
 
 
 function remove(arr, indexes) {
-    let arrayOfIndexes = [].slice.call(arguments, 1);  // (1)
-    return arr.filter(function (item, index) {         // (2)
-        return arrayOfIndexes.indexOf(index) === -1;      // (3)
+    let arrayOfIndexes = [].slice.call(arguments, 1);
+    return arr.filter(function (item, index) {
+        return arrayOfIndexes.indexOf(index) === -1;
     });
 }
 
@@ -52,13 +69,15 @@ window.onclick = function (event) {
 
 
 button_plus.addEventListener('click', () => {
+    textarea.value = ''
     const lishka = document.createElement('li');
     lishka.classList.add('active');
     const id = Date.now();
+    let date = new Date().toLocaleString()
     selected = id;
-    lishka.innerHTML = 'New note';
+    lishka.innerHTML = 'New note'+'<br>'+date;
     lishka.setAttribute('id', id)
-    const note = new Note(id, 'New note', '', true, new Date().toLocaleString());
+    const note = new Note(id, 'New note', '', true, date);
     notes.unshift(note);
     let ulka = document.getElementById('ulka');
     ulka.prepend(lishka);
@@ -93,9 +112,19 @@ button_minus.addEventListener('click', () => {
 });
 
 textarea.addEventListener('input', () => {
+    let date = new Date().toLocaleString()
     notes.forEach((elem) => {
         if (elem.isSelected === true) {
             elem.text = (textarea.value);
+            elem.date = date
+            elem.name = textarea.value.slice(0, 25)+'<br>'+ date
         }
     })
-});
+    let listLi = document.querySelectorAll('li');
+    listLi.forEach((elem) => {
+        if (elem.className === 'active') {
+            elem.innerHTML = textarea.value.slice(0, 25)+'<br>'+ date;
+        }
+    })
+
+})
